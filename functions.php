634d2687a2ab1b2cc44c064bff6f3939dbf245dd
Kenneth Lyerly <kenput3r@gmail.com>
2019-01-05 13:33:52 -0800

    change to get stylesheet directory uri

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@ function child_theme_enqueue_styles() {
     $parent_style = 'specia-standard';
     $child_style = 'specia-standard-child';
     wp_enqueue_style($parent_style, get_template_directory_uri() . '/style.css');
-    wp_enqueue_style($child_style, get_template_directory_uri() . '/style.css', array($parent_style), wp_get_theme()->get('Version'));
+    wp_enqueue_style($child_style, get_stylesheet_directory_uri() . '/style.css', array($parent_style), wp_get_theme()->get('Version'));
 }
 // add_action('wp_enqueue_scripts', 'child_theme_enqueue_scripts');
 // function child_theme_enqueue_scripts() {
