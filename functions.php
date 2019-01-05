583ff1c09f19f5c2b692b5f3dae420cbf0de0ea0
Ken Lyerly <kenput3r@Kens-Mac-Pro-Orange.local>
2019-01-05 13:14:02 -0800

    comment out enqueue js

--- functions.php
+++ functions.php
@@ -6,8 +6,8 @@ function child_theme_enqueue_styles() {
     wp_enqueue_style($parent_style, get_template_directory_uri() . '/style.css');
     wp_enqueue_style($child_style, get_template_directory_uri() . '/style.css', array($parent_style), wp_get_theme()->get('Version'));
 }
-add_action('wp_enqueue_scripts', 'child_theme_enqueue_scripts');
-function child_theme_enqueue_scripts() {
-    wp_euqueue_script('functions_js', get_template_directory_uri() . '/js/functions.js', array('jquery'), '1.0', true);
-}
+// add_action('wp_enqueue_scripts', 'child_theme_enqueue_scripts');
+// function child_theme_enqueue_scripts() {
+//     wp_euqueue_script('functions_js', get_template_directory_uri() . '/js/functions.js', array('jquery'), '1.0', true);
+// }
 ?>
