d39d1f8f4955059f25544d6caea99721a209741f
Kenneth Lyerly <kenput3r@gmail.com>
2019-01-06 10:02:19 -0800

    use jQuery to add and remove classes to service icons

--- js/functions.js
+++ js/functions.js
@@ -1,1 +1,9 @@
 console.log('Hello from functions.js');
+
+//if homepage, remove .specia-icon & .fa from service icons and replace with .specia-child-icon
+var is_home = $('body').hasClass('home');
+
+if(is_home) {
+    console.log('Welcom home');
+    $('.service-version-one .service-icon .specia-icon').addClass('specia-child-icon').removeClass('fa specia-icon');
+}
