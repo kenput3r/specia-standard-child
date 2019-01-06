345cf2870abd0caca28eb38fae3514cf7717cd39
Kenneth Lyerly <kenput3r@gmail.com>
2019-01-06 10:43:24 -0800

    add star class to i

--- js/functions.js
+++ js/functions.js
@@ -3,10 +3,10 @@ console.log('Hello from functions.js');
 jQuery.noConflict();
 (function($) {
     //if homepage, remove .specia-icon & .fa from service icons and replace with .specia-child-icon
+    //add star classes to blog section title
     var is_home = $('body').hasClass('home');
-
     if(is_home) {
-        console.log('Welcom home');
         $('.service-version-one .service-icon .specia-icon').addClass('specia-child-icon').removeClass('fa specia-icon');
+        $('.latest-blog .section-heading i').addClass('fas fa-star');
     }
 })(jQuery);
