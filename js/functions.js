2f32a782bd5a0a339259a11ec3dd551c04d3198c
Kenneth Lyerly <kenput3r@gmail.com>
2019-01-06 10:06:12 -0800

    wrap jQuery in noConflict function

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,12 @@
 console.log('Hello from functions.js');
 
-//if homepage, remove .specia-icon & .fa from service icons and replace with .specia-child-icon
-var is_home = $('body').hasClass('home');
+jQuery.noConflict();
+(function($) {
+    //if homepage, remove .specia-icon & .fa from service icons and replace with .specia-child-icon
+    var is_home = $('body').hasClass('home');
 
-if(is_home) {
-    console.log('Welcom home');
-    $('.service-version-one .service-icon .specia-icon').addClass('specia-child-icon').removeClass('fa specia-icon');
-}
+    if(is_home) {
+        console.log('Welcom home');
+        $('.service-version-one .service-icon .specia-icon').addClass('specia-child-icon').removeClass('fa specia-icon');
+    }
+})(jQuery);
